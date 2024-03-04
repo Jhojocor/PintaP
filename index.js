3bb39a5a41ad3a4802dfcbe0843b68a03a1122ac
Jhojocor <jhojocor@gmail.com>
2024-03-04 01:48:35 -0500

    Planteamiento de casillas por tipos listo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { Server } from 'socket.io';
-const PORT = 5050;
-const ip ='192.168.1.8'; //!cambia esto por la IP de tu pc
+const PORT = 8080;
+const ip ='192.168.0.7'; //!cambia esto por la IP de tu pc
 
 const app = express();
 const httpServer = app.listen(PORT, ip,  () => {
